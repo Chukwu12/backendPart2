0c06ea406d5651c814e70f3f5b03fce6feea38d3
okey E <okeysmooed@gmail.com>
2024-04-29 14:19:39 +0000

    update code

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+// Define a new token for logging request body
+morgan.token('reqBody', (req, res) => JSON.stringify(req.body));
 
 // Configure Morgan middleware to log messages to the console
-app.use(morgan('tiny'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -82,14 +84,26 @@ app.post('/api/persons', (req, res) => {
     if (nameExists) {
         return res.status(400).json({ error: 'Name must be unique' });
     }
+    // Generate a new ID
+    const newId = generateUniqueId();
     const newEntry = {
-        id: phonebookEntries.length + 1, // Generate ID based on length of phonebookEntries
+        id: newId,
         name: body.name,
         number: body.number
     };
     phonebookEntries.push(newEntry);
     res.json(newEntry);
 });
+
+// Function to generate a unique ID
+function generateUniqueId() {
+    // Define the range for the random ID
+    const min = 100000;
+    const max = 999999;
+    // Generate a random ID within the range
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 
 // Route to handle DELETE requests to "/api/persons/:id"
 app.delete('/api/persons/:id', (req, res) => {
@@ -106,8 +120,8 @@ app.delete('/api/persons/:id', (req, res) => {
     }
 });
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
   }
   
   app.use(unknownEndpoint)
@@ -117,6 +131,17 @@ const PORT = 3001;
 const server = app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+
+  app.use(requestLogger)
 
 // Close the server when the Node.js process is terminated
 process.on('SIGINT', () => {
